a74f61f6f4dbcb70f7f65431b2672cc6eba9e27c
ErickLima <ericklimacosta29@gmail.com>
2024-06-12 15:46:42 -0300

    inicializador

---
+++ src/main/java/com/ApiBilhete/ApiBilhetes/ApiBilhetesApplication.java
@@ -0,0 +1,13 @@
+package com.ApiBilhete.ApiBilhetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBilhetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBilhetesApplication.class, args);
+	}
+
+}
